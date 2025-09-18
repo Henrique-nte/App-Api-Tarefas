c24292a7567bbb4507316fdcd73db31ea2f46103
Henrique Santos <silvashrq@gmail.com>
2025-09-18 11:03:51 -0300

    Update script.js

--- front-end/script.js
+++ front-end/script.js
@@ -1,48 +1,45 @@
-
-
-
 async function getData() {
-    try {
-        const response = await fetch("http://localhost:3000/tasks");
+  try {
+    const response = await fetch("http://localhost:3000/tasks");
 
-        if (!response.ok) {
-            throw new Error(`Erro na response: ${response.status}`);
-        }
-
-        const data = response.json();
-
-        return data;
-
-    } catch (error) {
-        console.error("Ocorreu um erro", error.message);
+    if (!response.ok) {
+      throw new Error(`Erro na response: ${response.status}`);
     }
+
+    const data = response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Ocorreu um erro", error.message);
+  }
 }
 
 getData();
 
-const tasks = []
+const tasks = [];
 
 function addTasks(data) {
-
-    for (const task of data) {
-        tasks.push({ id: task.id, titulo: task.titulo, descricao: task.descricao, concluida: task.concluida });
-    }
-
+  for (const task of data) {
+    tasks.push({
+      id: task.id,
+      titulo: task.titulo,
+      descricao: task.descricao,
+      concluida: task.concluida,
+    });
+  }
 }
 
 function filtroTarefas() {
-    document.addEventListener("click", (event) => {
-        const button = event.target.id;
-        let listaTasks = document.getElementById("lista-tarefas");
+  document.addEventListener("click", (event) => {
+    const button = event.target.id;
+    let listaTasks = document.getElementById("lista-tarefas");
 
-        if (button === "filtro-todas") {
-            listaTasks.innerHTML = "";
+    if (button === "filtro-todas") {
+      listaTasks.innerHTML = "";
 
-            for (let i = 0; i < tasks.length; i++) {
-
-                if (tasks[i].concluida === true) {
-
-                    listaTasks.innerHTML += `
+      for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].concluida === true) {
+          listaTasks.innerHTML += `
                  <li class="tarefa concluida">
                      <div class="tarefa-info">
                          <h3>${tasks[i].titulo}</h3>
@@ -55,9 +52,8 @@ function filtroTarefas() {
                  </li>
                 
                 `;
-
-                } else {
-                    listaTasks.innerHTML += `
+        } else {
+          listaTasks.innerHTML += `
                      <li class="tarefa">
                          <div class="tarefa-info">
                              <h3>${tasks[i].titulo}</h3>
@@ -70,18 +66,16 @@ function filtroTarefas() {
                      </li>
                     
                     `;
-                }
-
-            }
-
         }
+      }
+    }
 
-        if (button === "filtro-pendentes") {
-            listaTasks.innerHTML = "";
+    if (button === "filtro-pendentes") {
+      listaTasks.innerHTML = "";
 
-            for (let i = 0; i < tasks.length; i++) {
-                if (tasks[i].concluida === false) {
-                    listaTasks.innerHTML += `
+      for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].concluida === false) {
+          listaTasks.innerHTML += `
                  <li class="tarefa">
                      <div class="tarefa-info">
                          <h3>${tasks[i].titulo}</h3>
@@ -94,18 +88,16 @@ function filtroTarefas() {
                  </li>
                 
                 `;
-                }
-
-            }
-
         }
+      }
+    }
 
-        if (button === "filtro-concluidas") {
-            listaTasks.innerHTML = "";
+    if (button === "filtro-concluidas") {
+      listaTasks.innerHTML = "";
 
-            for (let i = 0; i < tasks.length; i++) {
-                if (tasks[i].concluida === true) {
-                    listaTasks.innerHTML += `
+      for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].concluida === true) {
+          listaTasks.innerHTML += `
                  <li class="tarefa concluida">
                      <div class="tarefa-info">
                          <h3>${tasks[i].titulo}</h3>
@@ -118,60 +110,52 @@ function filtroTarefas() {
                  </li>
                 
                 `;
-                }
-
-            }
-
-
         }
-
-    });
+      }
+    }
+  });
 }
 
 async function sendNewTask(url, data) {
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-        if (!response.ok) {
-            throw new Error(`Erro na requisição: ${response.status}`);
-        }
-
-        const resultado = await resposta.json();
-        console.log('Sucesso:', resultado);
-
-        return resultado;
-
-    } catch (error) {
-        console.error('Falha na requisição:', error.message);
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
     }
+
+    const resultado = await resposta.json();
+    console.log("Sucesso:", resultado);
+
+    return resultado;
+  } catch (error) {
+    console.error("Falha na requisição:", error.message);
+  }
 }
 
 async function main() {
-    const data = await getData();
-    addTasks(data);
-    filtroTarefas();
-    // console.log(tasks);
+  const data = await getData();
+  addTasks(data);
+  filtroTarefas();
+  // console.log(tasks);
 
-    const formTask = document.getElementById("form-tarefa");
-    const tituloTask = formTask.querySelector("input").value;
-    const descTask = formTask.querySelector("textarea").value;
-    const button = formTask.querySelector("button");
+  const formTask = document.getElementById("form-tarefa");
+  const tituloTask = formTask.querySelector("input").value;
+  const descTask = formTask.querySelector("textarea").value;
+  const button = formTask.querySelector("button");
 
-    let url = "http://localhost:3000/tasks";
+  let url = "http://localhost:3000/tasks";
 
-    button.addEventListener("click", (event) => {
-        event.preventDefault();
-        sendNewTask(url, `{titulo: ${tituloTask}, descricao: ${descTask} }`)
-    });
-
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    sendNewTask(url, `{titulo: ${tituloTask}, descricao: ${descTask} }`);
+  });
 }
-
-
 
 main();
